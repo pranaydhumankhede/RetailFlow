b522a18962276e816ca46f9cc97c5a5b33ddcaf3
pranaydhumankhede <dhumankhedepranay@gmail.com>
2026-08-31 14:40:45 +0530

    Remove unnecessary password generator

--- billingsoftware/src/main/java/PasswordGenerator.java
+++
@@ -1,8 +0,0 @@
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-public class PasswordGenerator {
-    public static void main(String[] args) {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        System.out.println(encoder.encode("production"));
-    }
-}
